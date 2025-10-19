c9c0ae6e498d36b7833508b7c6663a892dff0867
replicaCortex <replicaCortex@gmail.com>
2025-10-19 22:26:21 +0300

    timer 0.0.1

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,55 @@
+use notify_rust::Notification;
+
+use std::time::{Duration, Instant};
 use std::{sync::mpsc, thread};
 
 use color_eyre::eyre::{Ok, Result};
+use ratatui::layout::{Alignment, Constraint, Layout};
+use ratatui::style::Modifier;
 use ratatui::{
     DefaultTerminal,
     buffer::Buffer,
     crossterm::event::{self, Event, KeyEvent},
-    layout::{Constraint, Layout, Rect},
-    style::{Color, Modifier, Style},
-    text::{Line, Span},
-    widgets::{Block, Paragraph, Tabs, Widget},
+    layout::Rect,
+    style::{Color, Style},
+    widgets::{Paragraph, Widget},
     *,
 };
-use strum::{Display, EnumIter, FromRepr, IntoEnumIterator};
+
+use clap::Parser;
+use tui_widgets::big_text::{BigText, PixelSize};
+
+#[derive(Parser, Debug)]
+#[command(version="0.0.1", author="replicaCortex",about ="Simple timer", long_about = None)]
+struct Cli {
+    /// Set the timer in minutes
+    #[arg(short = 'd', long, default_value_t = 5)]
+    duration: u16,
+
+    /// Notification Title
+    #[arg(short = 's', long, default_value_t = String::from(""))]
+    summary: String,
+
+    /// Main text of the notification
+    #[arg(short = 'b', long, default_value_t = String::from(""))]
+    body: String,
+}
 
 #[derive(Default, PartialEq, Eq)]
 enum TimerState {
-    #[default]
     Stop,
+    #[default]
     Running,
 }
 
-#[derive(Display, Default, Debug, EnumIter, FromRepr, Clone, Copy, PartialEq, Eq)]
+#[derive(Default, Debug, PartialEq)]
 enum AppState {
     #[default]
-    #[strum(to_string = " NORMAL ")]
     Normal,
-    #[strum(to_string = " INSERT ")]
-    Insert,
     Quit,
 }
 
-#[derive(Display, Default, Debug, EnumIter, FromRepr, Clone, Copy)]
+#[derive(Default, Debug)]
 enum Mode {
     #[default]
     Timer,
@@ -40,40 +59,30 @@ enum Mode {
 #[derive(Default)]
 struct App {
     current_time: chrono::TimeDelta,
+    startet_time: chrono::TimeDelta,
     timer_state: TimerState,
 
     app_state: AppState,
 
     mode: Mode,
+
+    send: bool,
+    notification_enable: bool,
+    send_notification: bool,
+
+    summary: String,
+    body: String,
 }
 
 enum AppEvent {
     Key(KeyEvent),
-    Pause,
-    Unpause,
     Tick,
 }
 
 fn main() -> Result<()> {
-    color_eyre::install()?;
-    let app = App {
-        current_time: chrono::Duration::seconds(10),
-        ..Default::default()
-    };
+    let (app, terminal) = init_app_and_terminal();
+    let rc = init_thread();
 
-    let (key_tx, rc) = mpsc::channel::<AppEvent>();
-
-    thread::spawn(move || {
-        loop {
-            if let Event::Key(key) = event::read().unwrap()
-                && key_tx.send(AppEvent::Key(key)).is_err()
-            {
-                break;
-            }
-        }
-    });
-
-    let terminal = ratatui::init();
     app.run(terminal, rc)?;
 
     ratatui::restore();
@@ -87,8 +96,7 @@ impl App {
 
             match rc.recv()? {
                 AppEvent::Key(key) => self.handle_key_event(key),
-                AppEvent::Tick => self.timer_update(),
-                _ => (),
+                AppEvent::Tick => self.update_timer(),
             }
         }
 
@@ -96,49 +104,64 @@ impl App {
     }
 
     fn is_running(&self) -> bool {
-        self.app_state == AppState::Normal || self.app_state == AppState::Insert
+        self.app_state == AppState::Normal
     }
 
+    fn update_timer(&mut self) {
+        if self.timer_state != TimerState::Stop {
+            if self.current_time - chrono::Duration::seconds(1) >= chrono::Duration::zero() {
+                self.current_time -= chrono::Duration::seconds(1);
+            } else {
+                self.notification();
+            }
+        }
+    }
+}
+
+impl App {
     fn handle_key_event(&mut self, key: KeyEvent) {
         match key.code {
             event::KeyCode::Char(' ') => {
-                if self.timer_state == TimerState::Running {
+                if self.current_time == chrono::Duration::zero() {
+                    self.current_time = self.startet_time;
+                } else if self.timer_state == TimerState::Running {
                     self.timer_state = TimerState::Stop;
                 } else {
                     self.timer_state = TimerState::Running;
                 }
             }
 
-            event::KeyCode::Char('i') => self.app_state = AppState::Insert,
-            event::KeyCode::Char('ш') => self.app_state = AppState::Insert,
+            event::KeyCode::Char('r') => self.reset(),
+            event::KeyCode::Char('к') => self.reset(),
 
-            event::KeyCode::Char('q') => self.quit(),
-            event::KeyCode::Char('й') => self.quit(),
-            event::KeyCode::Esc => self.quit(),
+            event::KeyCode::Char('q') => self.app_state = AppState::Quit,
+            event::KeyCode::Char('й') => self.app_state = AppState::Quit,
+            event::KeyCode::Esc => self.app_state = AppState::Quit,
 
-            event::KeyCode::Char('k') => {
-                self.mode = self.mode.previous();
-            }
-            event::KeyCode::Char('j') => {
-                self.mode = self.mode.next();
-            }
             _ => (),
         }
     }
 
-    fn timer_update(&mut self) {
-        if self.current_time > chrono::Duration::zero() {
-            self.current_time -= chrono::Duration::seconds(1);
-        } else {
-            self.timer_state = TimerState::Stop;
+    fn reset(&mut self) {
+        self.current_time = self.startet_time;
+
+        if self.notification_enable {
+            self.send = true;
         }
     }
 
-    fn quit(&mut self) {
-        if self.app_state == AppState::Insert {
-            self.app_state = AppState::Normal;
-        } else {
-            self.app_state = AppState::Quit;
+    fn notification(&mut self) {
+        if self.send {
+            if self.send_notification {
+                Notification::new()
+                    .summary(&self.summary)
+                    .body(&self.body)
+                    .timeout(notify_rust::Timeout::Milliseconds(10000))
+                    .show()
+                    .unwrap();
+            }
+
+            self.send = false;
         }
     }
 }
@@ -148,52 +171,65 @@ impl Widget for &App {
     where
         Self: Sized,
     {
-        let [top_area, bottom_area] =
-            Layout::horizontal([Constraint::Percentage(20), Constraint::Fill(1)]).areas(area);
+        let vertical_chunks = Layout::vertical([
+            Constraint::Fill(1),
+            Constraint::Length(8),
+            Constraint::Fill(1),
+        ])
+        .split(area);
 
-        self.render_title(top_area, buf);
-        self.render_mode(bottom_area, buf);
-        self.app_state.render(area, buf);
+        match self.mode {
+            Mode::Timer => self.render_timer(vertical_chunks[1], buf),
+            Mode::Alarm => self.render_alarm(area, buf),
+        }
     }
 }
 
 impl App {
-    fn render_title(&self, area: Rect, buf: &mut Buffer) {
-        let titles = Mode::iter().map(|mode| mode.to_string());
-        let tip = Block::new().title("Use j k to change mode ");
-        let selected_index = self.mode as usize;
-
-        Tabs::new(titles)
-            .block(tip)
-            .select(selected_index)
-            .padding("", "")
-            .divider(" ")
-            .render(area, buf);
-    }
-
-    fn render_mode(&self, area: Rect, buf: &mut Buffer) {
-        match self.mode {
-            Mode::Timer => self.render_timer(area, buf),
-            Mode::Alarm => self.render_alarm(area, buf),
-        }
-    }
-
     fn render_timer(&self, area: Rect, buf: &mut Buffer) {
         let total_seconds = self.current_time.num_seconds();
         let hours = total_seconds / 3600;
         let minutes = (total_seconds % 3600) / 60;
         let seconds = total_seconds % 60;
 
-        let style = Style {
-            fg: match self.timer_state {
-                TimerState::Stop => Some(Color::Gray),
-                TimerState::Running => Some(Color::LightGreen),
-            },
-            ..Default::default()
+        let style = {
+            if self.current_time != chrono::Duration::zero() {
+                Style {
+                    fg: match self.timer_state {
+                        TimerState::Stop => Some(Color::Gray),
+                        TimerState::Running => Some(Color::LightYellow),
+                    },
+                    ..Default::default()
+                }
+            } else {
+                Style {
+                    fg: match self.timer_state {
+                        TimerState::Stop => Some(Color::Gray),
+                        TimerState::Running => Some(Color::LightYellow),
+                    },
+                    ..Default::default()
+                }
+                .add_modifier(Modifier::SLOW_BLINK)
+            }
         };
-        let span = Span::styled(format!("{:02}:{:02}:{:02}", hours, minutes, seconds), style);
+        let lines = {
+            if hours > 0 {
+                format!("{:02}:{:02}:{:02}", hours, minutes, seconds)
+            } else if minutes > 0 {
+                format!("{:02}:{:02}", minutes, seconds)
+            } else {
+                format!("{:02}", seconds)
+            }
+        };
 
-        Line::from(span).render(area, buf);
+        let big_text = BigText::builder()
+            .pixel_size(PixelSize::Full)
+            .style(style)
+            .lines(vec![lines.into()])
+            .alignment(Alignment::Center)
+            .build();
+
+        big_text.render(area, buf);
     }
 
     fn render_alarm(&self, area: Rect, buf: &mut Buffer) {
@@ -201,40 +237,61 @@ impl App {
     }
 }
 
-impl Mode {
-    fn next(self) -> Self {
-        let current_index = self as usize;
-        let next_index = current_index.saturating_add(1);
-        Self::from_repr(next_index).unwrap_or(self)
-    }
+fn init_app_and_terminal() -> (App, DefaultTerminal) {
+    color_eyre::install().unwrap();
 
-    fn previous(self) -> Self {
-        let current_index = self as usize;
-        let previous_index = current_index.saturating_sub(1);
-        Self::from_repr(previous_index).unwrap_or(self)
-    }
+    let cli = Cli::parse();
+
+    let terminal = ratatui::init();
+    let mut app = App {
+        current_time: chrono::Duration::seconds((cli.duration * 60) as i64),
+        notification_enable: { !cli.summary.is_empty() || !cli.body.is_empty() },
+        send: { !cli.summary.is_empty() || !cli.body.is_empty() },
+
+        send_notification: { !cli.summary.is_empty() || !cli.body.is_empty() },
+        summary: cli.summary,
+        body: cli.body,
+
+        ..Default::default()
+    };
+
+    app.startet_time = app.current_time;
+
+    (app, terminal)
 }
 
-impl Widget for &AppState {
-    fn render(self, area: Rect, buf: &mut Buffer)
-    where
-        Self: Sized,
-    {
-        let title = self.to_string();
-        let style = Style {
-            fg: Some(Color::Black),
-            bg: match self {
-                AppState::Normal => Some(Color::LightBlue),
-                AppState::Insert => Some(Color::Magenta),
-                _ => Some(Color::Black),
-            },
-            add_modifier: Modifier::BOLD,
-            ..Default::default()
-        };
-        let span = Span::styled(&title, style);
+fn init_thread() -> mpsc::Receiver<AppEvent> {
+    let (tx, rc) = mpsc::channel::<AppEvent>();
+    let key_tx = tx.clone();
+    thread::spawn(move || {
+        loop {
+            if let Event::Key(key) = event::read().unwrap()
+                && key_tx.send(AppEvent::Key(key)).is_err()
+            {
+                break;
+            }
+        }
+    });
 
-        let area = Rect::new(0, area.height - 1, title.len() as u16, 2);
+    let tick_tx = tx;
+    thread::spawn(move || {
+        let tick_rate = Duration::from_secs(1);
+        let mut last_tick = Instant::now();
 
-        Line::from(span).render(area, buf);
-    }
+        loop {
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+
+            thread::sleep(timeout);
+
+            last_tick = Instant::now();
+
+            if tick_tx.send(AppEvent::Tick).is_err() {
+                break;
+            }
+        }
+    });
+
+    rc
 }
